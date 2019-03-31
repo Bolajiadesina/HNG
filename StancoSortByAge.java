36ed2d29d8fdf237422f55e3825bdae2cc7e78d2
Bolaji Adesina <39880314+Bolajiadesina@users.noreply.github.com>
2019-03-31 23:08:23 +0100

    Add files via upload

---
+++ StancoSortByAge.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stancouniv;
+
+import java.util.Comparator;
+
+/**
+ *
+ * @author ACER PC
+ */
+public class StancoSortByAge implements Comparator <StancoUni> {
+
+    @Override
+    public int compare(StancoUni s1 StancoUni s2) {
+        //if (s1.getAge() > s2.getAge()) {
+        //    return 1;
+       // } else if (s1.getAge() > s2.getAge()) {
+         //   return -1;
+       // } else {
+        //    return 0;
+       // }
+
+   // }
+    
+}
